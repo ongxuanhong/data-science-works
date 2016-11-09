5dc9d75ca3b440c1536f5efc0bf49425833c53a7
Hong Ong <hong_ong@knorex.com>
2016-11-09 12:43:28 +0800

    Linear regression regularization

--- python/regression_algorithms.py
+++ python/regression_algorithms.py
@@ -16,6 +16,7 @@ import os
 import pandas as pd
 from sklearn import linear_model
 from sklearn.externals import joblib
+from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 
 
@@ -39,13 +40,38 @@ if __name__ == "__main__":
     # split dataset into training (70%) and testing set (30%)
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
+    """
+    DEFAULT MODEL
+    """
     # training model
     linear = linear_model.LinearRegression()
     linear.fit(x_train, y_train)
 
     # evaluating model
-    score_trained = linear.score(x_train, y_train)
+    score_trained = linear.score(x_test, y_test)
     print "Model scored:", score_trained
+
+    """
+    LASSO MODEL
+    """
+    # L1 regularization
+    lasso_linear = linear_model.Lasso(alpha=1.0)
+    lasso_linear.fit(x_train, y_train)
+
+    # evaluating L1 regularized model
+    score_lasso_trained = lasso_linear.score(x_test, y_test)
+    print "Lasso model scored:", score_lasso_trained
+
+    """
+    RIDGE MODEL
+    """
+    # L2 regularization
+    ridge_linear = Ridge(alpha=1.0)
+    ridge_linear.fit(x_train, y_train)
+
+    # evaluating L2 regularized model
+    score_ridge_trained = ridge_linear.score(x_test, y_test)
+    print "Ridge model scored:", score_ridge_trained
 
     # saving model
     joblib.dump(linear, "models/linear_model_v1.pkl")
