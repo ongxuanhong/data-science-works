e6408ee7ba431cf969127922c4864e889e798a1c
Hong Ong <hong_ong@knorex.com>
2016-12-03 11:09:35 +0700

    Uncomment

--- python/sentiment_analysis/classification_algorithms.py
+++ python/sentiment_analysis/classification_algorithms.py
@@ -16,11 +16,24 @@ import os
 import re
 import time
 from itertools import islice
+from operator import itemgetter
 
 import numpy as np
 import pandas as pd
 from BeautifulSoup import BeautifulSoup
 from nltk.corpus import stopwords
+from sklearn.cross_validation import train_test_split
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 
 def time_diff_str(t1, t2):
@@ -163,82 +176,83 @@ if __name__ == "__main__":
     convert_plain_to_csv("foods.txt", "foods.csv")
 
     # Reading the Data
-    # train = get_reviews_data("foods.csv")
-    # print "Data dimensions:", train.shape
-    # print "List features:", train.columns.values
-    # print "First review:", train["summary"][0], "|", train["text"][0]
-    #
-    # cleaning_data(train, "clean_train_reviews.csv")
-    #
-    # """
-    # Bag of Words features
-    # """
-    #
-    # clean_train_reviews = pd.read_csv("clean_train_reviews.csv", nrows=1000)
-    #
-    # # ignore all 3* reviews
-    # clean_train_reviews = clean_train_reviews[clean_train_reviews["score"] != 3]
-    # # positive sentiment = 4* or 5* reviews
-    # clean_train_reviews["sentiment"] = clean_train_reviews["score"] >= 4
-    #
-    # train, test = train_test_split(clean_train_reviews, test_size=0.2)
-    #
-    # print "Creating the bag of words...\n"
-    # vectorizer = CountVectorizer(analyzer="word",
-    #                              tokenizer=None,
-    #                              preprocessor=None,
-    #                              stop_words=None,
-    #                              max_features=10)
-    #
-    # train_text = train["text"].values.astype('U')
-    # test_text = test["text"].values.astype('U')
-    #
-    # # convert data-set to term-document matrix
-    # X_train = vectorizer.fit_transform(train_text).toarray()
-    # y_train = train["sentiment"]
-    #
-    # X_test = vectorizer.fit_transform(test_text).toarray()
-    # y_test = test["sentiment"]
-    #
-    # print_words_frequency(train_data_features)
-    #
-    # """
-    # Training
-    # """
-    #
-    # print "---------------------------"
-    # print "Training"
-    # print "---------------------------"
-    #
-    # names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
-    #          "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
-    #          "Naive Bayes", "QDA"]
-    #
-    # classifiers = [
-    #     KNeighborsClassifier(3),
-    #     SVC(kernel="linear", C=0.025),
-    #     SVC(gamma=2, C=1),
-    #     GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True),
-    #     DecisionTreeClassifier(max_depth=5),
-    #     RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
-    #     MLPClassifier(alpha=1),
-    #     AdaBoostClassifier(),
-    #     GaussianNB(),
-    #     QuadraticDiscriminantAnalysis()]
-    #
-    # # iterate over classifiers
-    # results = {}
-    # for name, clf in zip(names, classifiers):
-    #     print "Training " + name + " classifier..."
-    #     clf.fit(X_train, y_train)
-    #     score = clf.score(X_test, y_test)
-    #     results[name] = score
-    #
-    # print "---------------------------"
-    # print "Evaluation results"
-    # print "---------------------------"
-    #
-    # # sorting results and print out
-    # sorted(results.items(), key=itemgetter(1))
-    # for name in results:
-    #     print name + " accuracy: %0.3f" % results[name]
+    train = get_reviews_data("foods.csv")
+    print "Data dimensions:", train.shape
+    print "List features:", train.columns.values
+    print "First review:", train["summary"][0], "|", train["text"][0]
+
+    cleaning_data(train, "clean_train_reviews.csv")
+
+    """
+    Bag of Words features
+    """
+
+    clean_train_reviews = pd.read_csv("clean_train_reviews.csv", nrows=1000)
+
+    # ignore all 3* reviews
+    clean_train_reviews = clean_train_reviews[clean_train_reviews["score"] != 3]
+    # positive sentiment = 4* or 5* reviews
+    clean_train_reviews["sentiment"] = clean_train_reviews["score"] >= 4
+
+    train, test = train_test_split(clean_train_reviews, test_size=0.2)
+
+    print "Creating the bag of words...\n"
+    vectorizer = CountVectorizer(analyzer="word",
+                                 tokenizer=None,
+                                 preprocessor=None,
+                                 stop_words=None,
+                                 max_features=10)
+
+    train_text = train["text"].values.astype('U')
+    test_text = test["text"].values.astype('U')
+
+    # convert data-set to term-document matrix
+    X_train = vectorizer.fit_transform(train_text).toarray()
+    y_train = train["sentiment"]
+
+    X_test = vectorizer.fit_transform(test_text).toarray()
+    y_test = test["sentiment"]
+
+    print_words_frequency(X_train)
+
+    """
+    Training
+    """
+
+    print "---------------------------"
+    print "Training"
+    print "---------------------------"
+
+    names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
+             "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
+             "Naive Bayes", "QDA"]
+
+    classifiers = [
+        KNeighborsClassifier(3),
+        SVC(kernel="linear", C=0.025),
+        SVC(gamma=2, C=1),
+        GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True),
+        DecisionTreeClassifier(max_depth=5),
+        RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+        MLPClassifier(alpha=1),
+        AdaBoostClassifier(),
+        GaussianNB(),
+        QuadraticDiscriminantAnalysis()]
+
+    # iterate over classifiers
+    results = {}
+
+    for name, clf in zip(names, classifiers):
+        print "Training " + name + " classifier..."
+        clf.fit(X_train, y_train)
+        score = clf.score(X_test, y_test)
+        results[name] = score
+
+    print "---------------------------"
+    print "Evaluation results"
+    print "---------------------------"
+
+    # sorting results and print out
+    sorted(results.items(), key=itemgetter(1))
+    for name in results:
+        print name + " accuracy: %0.3f" % results[name]
