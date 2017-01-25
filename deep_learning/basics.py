ef9b5467c52ae201f85b84797674a2425b08ccf3
Hong Ong <hong_ong@knorex.com>
2017-01-25 10:05:02 +0700

    Working with DICOM images

--- deep_learning/basics.py
+++ deep_learning/basics.py
@@ -1,5 +1,10 @@
+import glob
+import os
+
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+import numpy as np
+from pydicom import dicomio
 
 if __name__ == "__main__":
     img = mpimg.imread("imgs/dogs.jpg")
@@ -17,4 +22,29 @@ if __name__ == "__main__":
     plt.imshow(img[:, :, 1], cmap='gray')
     plt.show()
     plt.imshow(img[:, :, 2], cmap='gray')
+    plt.show()
+
+    root_dir = "sample_images/00cba091fa4ad62cc3200a657aeb957e/"
+    os.chdir(root_dir)
+    images = []
+    for f in glob.glob("*.dcm"):
+        ds = dicomio.read_file(f)
+        img = ds.pixel_array
+        images.append(img)
+
+    # convert to array
+    data = np.array(images)
+    print "Total images:", len(images)
+    print "Image dimensions:", images[0].shape
+    print "Combine dimensions:", data.shape
+
+    plt.style.use('ggplot')
+    print "Calculating mean images"
+    mean_img = np.mean(data, axis=0)
+    plt.imshow(mean_img.astype(np.uint8))
+    plt.show()
+
+    print "Calculating deviation images"
+    std_img = np.std(data, axis=0)
+    plt.imshow(std_img.astype(np.uint8))
     plt.show()
