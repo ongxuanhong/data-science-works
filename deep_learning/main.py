4c177e6df079041e3e03f52e29a40e10d1005528
Hong Ong <hong_ong@knorex.com>
2017-02-09 15:04:48 +0700

    Image segmentation

---
+++ deep_learning/main.py
@@ -0,0 +1,64 @@
+import argparse
+import datetime
+import glob
+import os
+import sys
+import time
+
+import numpy as np
+
+sys.path.append("/usr/local/lib/python2.7/site-packages")
+import cv2
+import dicom as dicomio
+
+
+def time_diff_str(t1, t2):
+    """
+    Calculates time durations.
+    """
+    diff = t2 - t1
+    mins = int(diff / 60)
+    secs = round(diff % 60, 2)
+    return str(mins) + " mins and " + str(secs) + " seconds"
+
+
+if __name__ == "__main__":
+    t_start = time.time()
+
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--dataset", required=True, help="path to input dataset")
+    ap.add_argument("-s", "--saveto", required=True, help="path to saved processed data")
+    args = vars(ap.parse_args())
+
+    patients_images_dict = {}
+    list_dir = os.listdir(args["dataset"])
+    for (i, dir) in enumerate(list_dir):
+        if os.path.isfile(dir) is False:
+            basePath = args["dataset"] + "/" + dir
+            os.chdir(basePath)
+            images = []
+            for f in glob.glob("*.dcm"):
+                # read dcm file
+                ds = dicomio.read_file(f)
+                img = ds.pixel_array
+
+                # normalize image values to [0, 255]
+                cv2.normalize(img, img, 0, 255, cv2.NORM_MINMAX)
+
+                # image segmentation
+                ret, thresh = cv2.threshold(img.astype(np.uint8), 127, 255, cv2.THRESH_TOZERO)
+                images.append(thresh)
+
+            data = np.array(images)
+            mean_img = np.mean(data, axis=0)
+            save_name = args["saveto"] + "/" + dir + ".png"
+            cv2.imwrite(save_name, mean_img)
+            print "Saved processed image:", save_name
+
+        # show an update every 10 patients
+        if i > 0 and i % 10 == 0:
+            print "[INFO] processed {}/{} patients".format(i, len(list_dir))
+
+    print len(patients_images_dict)
+    print "[INFO]", datetime.datetime.now(), "* DONE After *", time_diff_str(t_start, time.time())
