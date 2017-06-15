6104e479ca34bd0499d8491830a11a1c8fa59b26
Hong Ong <hong_ong@knorex.com>
2017-06-15 12:10:37 +0800

    Add bar chart

--- sma2017/etl_data.py
+++ sma2017/etl_data.py
@@ -46,6 +46,21 @@ def line_chart(plt, x, y):
     plt.show()
 
 
+def bar_chart(plt, x, y):
+    # bars are by default width 0.8, so we'll add 0.1 to the left coordinates
+    # so that each bar is centered
+    xs = [i + 0.1 for i, _ in enumerate(x)]
+
+    # plot bars with left x-coordinates [xs], heights [y]
+    plt.bar(xs, y, color="#f39c12")
+    plt.title("Clicks statistic")
+
+    # label x-axis with color names at bar centers
+    plt.xticks([i + 0.5 for i, _ in enumerate(x)], x)
+
+    plt.show()
+
+
 if __name__ == "__main__":
     t_start = time.time()
     print "------------------------------------------------"
@@ -58,8 +73,10 @@ if __name__ == "__main__":
 
     # params
     widget_ids = ["knxad_knx2991_201604204777"]
+    section = ["ea845d619a665959b6a6d7415457479a55cd4149"]
     coll_brand_display_analytics = db_brand_display_analytics["brand_display" + "_05_2017"]
 
+    idx = 0
     for item in widget_ids:
         print "* Process widget:", item
 
@@ -71,8 +88,11 @@ if __name__ == "__main__":
                 "$gte": 1493654400,
                 "$lt": 1493740800
             },
-            "section": "ea845d619a665959b6a6d7415457479a55cd4149"
+            "section": section[idx]
         })
+        # next item
+        idx += 1
+
         df_report_items = pd.DataFrame(list(cursor_report))
         df_report_items = df_report_items.sort_values(by=["date"], ascending=[1])
 
@@ -101,5 +121,6 @@ if __name__ == "__main__":
 
         print "Number items:", num_items
         line_chart(plt, x, y)
+        # bar_chart(plt, x, y)
 
     print " %s * DONE After * %s" % (datetime.datetime.now(), time_diff_str(t_start, time.time()))
