89ac941971a95b14632fd50e35dd814d503e690a
Hong Ong <hong_ong@knorex.com>
2017-06-15 16:48:56 +0800

    Add confusion matrix

--- sma2017/demo_classification.py
+++ sma2017/demo_classification.py
@@ -1,14 +1,18 @@
 import csv
 import datetime
+import itertools
 import time
 from operator import itemgetter
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import RBF
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
@@ -27,6 +31,40 @@ def time_diff_str(t1, t2):
     return str(mins) + " mins and " + str(secs) + " seconds"
 
 
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
 if __name__ == "__main__":
     t_start = time.time()
     print "------------------------------------------------"
@@ -35,6 +73,8 @@ if __name__ == "__main__":
 
     df_data = pd.read_csv("data/knxad_knx2991_201604204777.csv")
     features = list(df_data.columns[:4])
+    class_names = [-1, 0, 1]
+
     y = df_data["decision"]
     X = df_data[features]
 
@@ -55,7 +95,7 @@ if __name__ == "__main__":
 
     classifiers = [
         KNeighborsClassifier(3),
-        SVC(kernel="linear", C=0.025),
+        SVC(kernel="sigmoid", C=0.025),
         SVC(gamma=2, C=1),
         GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True),
         DecisionTreeClassifier(max_depth=5),
@@ -73,6 +113,18 @@ if __name__ == "__main__":
         clf.fit(X_train, y_train)
         score = clf.score(X_test, y_test)
         results[name] = score
+
+        # Compute confusion matrix
+        y_pred = clf.predict(X_test)
+        cnf_matrix = confusion_matrix(y_test, y_pred)
+        np.set_printoptions(precision=2)
+
+        # Plot normalized confusion matrix
+        plt.figure()
+        plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+                              title='Normalized confusion matrix')
+
+        plt.savefig("figs/" + name)
 
     print "---------------------------"
     print "Evaluation results"
